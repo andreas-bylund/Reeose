7ce58ab3ebe2d742b39575c8c21b1a52e9a1f77c
Andreas Bylund <andreas@andreasbylund.se>
2016-10-01 01:21:31 +0200

    Ändrar base_url

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'https://reeo.se';
-$config['base_url'] = 'http://localhost/design';
+$config['base_url'] = 'http://localhost/reeose';
 
 /*
 |--------------------------------------------------------------------------
